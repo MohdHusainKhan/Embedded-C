eec0f2654ffe16e7c3b75684425d29e7dfea76f5
Mohd Husain Mohd Zama Khan <64732412+MohdHusainKhan@users.noreply.github.com>
2021-04-27 20:15:31 +0530

    Add files via upload

---
+++ inc/Activity1.h
@@ -0,0 +1,29 @@
+
+/**
+ * @file pin_config.h
+ * @author Husain Khan
+ * @brief Header file for defining pin config() function
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef ACTIVITY1_H_INCLUDED
+#define ACTIVITY1_H_INCLUDED
+
+
+/**
+ * @brief Will define configuration of several pins.
+ *
+ */
+void pin_config();
+
+/**
+ * @brief Function will check the status of heater button and passenger seat.
+ *
+ */
+int system_init();
+
+#endif // ACTIVITY1_H_INCLUDED
