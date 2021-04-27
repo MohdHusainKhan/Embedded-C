cb3cddeae6f7fdd113cc8e42188bdec0a9740c95
Mohd Husain Mohd Zama Khan <64732412+MohdHusainKhan@users.noreply.github.com>
2021-04-27 20:20:48 +0530

    Create SeatHeatingApp.c

---
+++ SeatHeatingApp.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../inc/Activity1.h"
+
+int main(void)
+{
+    //Calling the system_init() function from Activity1.c
+    system_init();
+
+    return 0;
+}
+
